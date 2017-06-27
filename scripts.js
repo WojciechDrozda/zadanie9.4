9a999aeaa801b532748f83e0b9e96cd6173edabf
Wojciech Drozda <wojciech.drozdaa@gmail.com>
2017-06-27 16:49:59 +0200

    test

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function drawTree(treeHeight) {
+    for ( var i = 1 ; i < 6 ; i++ ) {
+    var star = '';
+   for ( var j = 1 ; j <= i ; j++ ) {
+     star += '*';
+   }
+    console.log(star);
+   }
+}
+  drawTree();
